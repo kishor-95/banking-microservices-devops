c3c827f8a63a39f2da2b54cff3b6a92ed925b030
Kishor Bhairat <bhairatkishor02@gmail.com>
2026-06-30 12:49:14 +0530

    refactor(dependencies): reorganize package.json for better readability

--- balance-service/main.py
+++ balance-service/main.py
@@ -4,7 +4,7 @@ import time
 
 import psycopg2
 import psycopg2.extras
-from fastapi import FastAPI, HTTPException, Depends, Request, security
+from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from metrics import (
